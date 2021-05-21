da3036142e3ec1d431a44c6ae16acbabb03da957
kurwitz3 <kurwitz3@aol.com>
2021-05-20 20:05:09 -0400

    commit tree 51f2c088fff665aa8b47b4fa54cfe421902fa023

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-// Code your solution here!
+function printString(string){
+    console.log(string[0]);
+
+	if (string.length > 1) {
+		let mySubString = string.substring(1, string.length);
+		printString(mySubString);
+	} else {
+		return true;
+	}
+}
+
+function reverseString(string){
+    console.log(string);
+
+	if (string === '') {
+		return ''
+	} else {
+		return reverseString(string.substring(1)) + string[0]
+	}
+}
+
+function isPalindrome(str){
+    const strLen = str.length;
+    if (strLen < 2) return true;
+
+    if (str[0] === str[strLen - 1]) {
+        return isPalindrome( str.slice(1, strLen - 1) );
+    }
+
+    return false;
+
+}
+
+function addUpTo(index,array){
+   return array[index] + addUpTo(array,index -1)
+    
+  
+}
 
